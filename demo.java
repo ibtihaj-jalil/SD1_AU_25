f457959f96bf63753acb9dbd8b680c78faeef9d5
ibtihaj-jalil <ibtihajjalil@gmail.com>
2025-09-01 21:17:19 +0600

    Add demo.java with input example

---
+++ demo.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class demo {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+
+System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        System.out.print("Enter your salary: ");
+        double salary = sc.nextDouble();
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+    }
+}
