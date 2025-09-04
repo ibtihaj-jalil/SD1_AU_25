baad9b1c544b32f8ae66e3e91d53236b7f888303
ibtihaj_jalil <161217744+ibtihaj-jalil@users.noreply.github.com>
2025-09-04 23:00:44 +0600

    Update problem_5.java

--- Basic_Java_Problem/problem_5.java
+++ Basic_Java_Problem/problem_5.java
@@ -1,10 +1,46 @@
 import java.util.*;
 public class problem_5 {
-    public static void main(String [] args){
-        Scanner input=new Scanner (System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        int x=input.nextInt();
-        int y=input.nextInt();
-        
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        char op = sc.next().charAt(0);
+
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        double result = 0;
+
+        switch(op) {
+            case '+':
+                result = num1 + num2;
+                break;
+
+            case '-':
+                result = num1 - num2;
+                break;
+
+            case '*':
+                result = num1 * num2;
+                break;
+
+            case '/':
+                if(num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Division by zero.");
+                    return;
+                }
+                break;
+
+            default:
+                System.out.println("Invalid operator! only use +,-,*,/");
+                return;
+        }
+
+        System.out.println("Result: " + result);
     }
 }
